33ddf53d44aac69dc6b7a6972c185b7187245a61
Shamardn <waelshamardn@yahoo.com>
2021-10-18 01:31:00 +0200

    added new list item card

--- app/src/main/java/com/shamardn/android/inventory/OutfitCursorAdapter.java
+++ app/src/main/java/com/shamardn/android/inventory/OutfitCursorAdapter.java
@@ -17,7 +17,7 @@ public class OutfitCursorAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item_card,parent,false);
         return view;
     }
 
